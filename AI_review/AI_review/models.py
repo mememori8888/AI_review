7284246ad5d3c1643cc6a5f815acbfdc245983c5
mememori8888 <42532462+mememori8888@users.noreply.github.com>
2025-03-12 19:58:38 +0900

    mysqlの内容を表示できた

--- AI_review/AI_review/models.py
+++ AI_review/AI_review/models.py
@@ -4,7 +4,11 @@ import datetime
 class AIReview(models.Model):
     question = models.TextField()
     response = models.TextField()
-    timestamp = models.DateTimeField(default=datetime.datetime.now)
+    timestamp = models.DateTimeField()
+
+    class Meta:
+        managed = False  # 既存のテーブルを使用することを示す
+        db_table = 'responses'  # 実際のテーブル名を指定
 
 def __str__(self):
     return self.question[:50] # 表示用に短縮
